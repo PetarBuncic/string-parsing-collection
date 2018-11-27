d027f14d778227db6b6d6834267e19fc60909fea
PetarBuncic <44311324+PetarBuncic@users.noreply.github.com>
2018-11-28 00:17:01 +0100

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,21 +1,25 @@
+package string.colection;
 import java.time.LocalDate;
-import java.util.Date;
 
+/*Definisemo klasu Person koja ce da cuva podatke o osobi, svaki objekat ove klase ima 
+ * atribute(ime, prezime,mestorodjenja i datum rodjenja)*/
 public class Person{
     public String ime;
     public String prezime;
     public String mestorodjenja;
-    public String datumrodjenja;
-
-    public Person(String ime, String prezime, String datumrodjenja,String mestorodjenja ){
+    public LocalDate birthdate;
+    
+/*Definisemo nas konstruktor koji prima argumente kao u postavci zadatka (4 atributa, 3 stringa i LocalDate)*/
+    public Person(String ime, String prezime,LocalDate birthdate,String mestorodjenja ){
         this.ime=ime;
         this.prezime=prezime;
-        this.datumrodjenja=datumrodjenja;
         this.mestorodjenja=mestorodjenja;
+        this.birthdate=birthdate;
     }
-    
+    /*Kako bi nam kasnije bilo lakse da stampamo sadrzaje nasih objekata(Persona(Osoba)) implementiramo jednu 
+     * metodu koja stampa nase objekte*/
     public void displayPerson() {
-    	System.out.println("Ime: "+this.ime +" "+"Prezime: "+this.prezime +" "+"Datum rodjenja: " +this.datumrodjenja +" "+"Mesto rodjenja: "+this.mestorodjenja);
+    	System.out.println("Ime: "+this.ime +" "+"Prezime: "+this.prezime + " " +"Mesto rodjenja: "+this.mestorodjenja+" "+"Datum rodjenja: "+this.birthdate.getDayOfMonth()+"."+this.birthdate.getMonth()+" "+ this.birthdate.getYear()+".god");
+    
     }
-
 }
